e895ec7496af59f251174efb3d9d41d3bb26938a
gabliltraydev <99366409+gabliltraydev@users.noreply.github.com>
2023-10-28 02:25:39 +0200

    i tested
    ill soon add other things such as an better obsidian block protected by rhino horn and other cool stuff,stay tuned :)

--- src/main/java/fr/mysticraft/mystimod/event/ModEventBusClientEvents.java
+++ src/main/java/fr/mysticraft/mystimod/event/ModEventBusClientEvents.java
@@ -12,6 +12,8 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = MystiMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModEventBusClientEvents {
 
+    
+
     @SubscribeEvent
     public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions e){
         e.registerLayerDefinition(ModModelLayers.RHINO_LAYER, RhinoModel::createBodyLayer);
